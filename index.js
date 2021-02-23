8c7aef08f71be19ae7bdfaaa4bdefdaf21a82443
Adnan Aslam <adnanaslam27j@gmail.com>
2021-02-24 04:53:01 +0530

    test cases find in readme file

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ import "./style.css";
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = Solution("coders have no life");
 
-// Reverse string with built-in functions
+// Reverse string with built-in functions : Method 1
 function Solution(str) {
   let newString = "";
   let len = str.length;
-
+  
   for (let i = len - 1; i >= 0; i--) {
     newString += str[i];
   }
   return newString;
 }
 
-// Reverse a string with for Loop
+// Reverse a string with for Loop : Method 2
 function Solution(str) {
   return str
     .split("")
@@ -23,23 +23,23 @@ function Solution(str) {
     .join("");
 }
 
-// Reverse a string by using stack method push and pop
-function Solution(str) {
-  let stack = [];
-  let len = str.length;
-  // push() letter in the stack
-  for (let i = 0; i < len; i++) {
-    stack.push(str[i]);
+// Reverse a string by using stack method push and pop : Method 3
+  function Solution(str){
+    let stack = [];
+    let len = str.length;
+    // push() letter in the stack
+    for(let i = 0; i < len; i++){
+       stack.push(str[i]);
+    }
+    // push() letter from the stack
+    let reverseString = "";
+    while (stack.length > 0){
+      reverseString += stack.pop();
+    }
+    return reverseString;
   }
-  // push() letter from the stack
-  let reverseString = "";
-  while (stack.length > 0) {
-    reverseString += stack.pop();
-  }
-  return reverseString;
-}
 
-// Reverse a string with Recursion Method
+// Reverse a string with Recursion Method : Method 4
 function Solution(str) {
   if (str === "") {
     return "";
@@ -47,8 +47,3 @@ function Solution(str) {
     return Solution(str.substr(1)) + str.charAt();
   }
 }
-
-//Test cases
-// Solution("coders") return "sredoc"
-// Solution("have no life") return "efil on evah"
-// Solution("coders have no life") return "efil on evah sredoc”
